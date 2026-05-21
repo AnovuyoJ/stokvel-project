cc8ce31c4d7bc5e54ba9edfad2f5d4defd337db1
Anovuyo Dlamini <anovuyojafta@gmail.com>
2026-05-22 01:15:10 +0200

    Comment out FALLBACK_RATES in useRates.js
    Comment out the FALLBACK_RATES constant in useRates.js

--- client/src/utils/useRates.js
+++ client/src/utils/useRates.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 const API = import.meta.env.VITE_API_URL;
 
-const FALLBACK_RATES = { repoRate: 8.25, primeRate: 11.75 };
+// const FALLBACK_RATES = { repoRate: 8.25, primeRate: 11.75 };
 
 export function useRates() {
   const [rates, setRates] = useState(null);
